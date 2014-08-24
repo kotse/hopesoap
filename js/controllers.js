5d2e28418646d5fbe838dd06ca9f9c7f2018a3bd
Kostadin Golev <kotseto@gmail.com>
2014-08-24 17:26:10 +0300

    order by options added to the catalog

--- js/controllers.js
+++ js/controllers.js
@@ -3,10 +3,13 @@ var hopesoapApp = angular.module('hopeSoapApp', []);
 hopesoapApp.controller('SoapListCtrl', function ($scope) {
   $scope.soaps = [
     {'name': 'Luxury',
-     'description': 'Луксозни сапуни за вашия специален повод'},
+     'description': 'Луксозни сапуни за вашия специален повод',
+ 	 'createdAt': new Date('12/23/2013')},
     {'name': 'Christmas special',
-     'description': 'Коледа'},
+     'description': 'Коледа',
+ 	 'createdAt': new Date('11/24/2013')},
     {'name': 'Classic',
-     'description': 'Класически модели сапуни'},
+     'description': 'Класически модели сапуни',
+ 	 'createdAt': new Date('07/08/2014')},
   ];
 });
