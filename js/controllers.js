baaa4a0b4cfb137240b46961420b86cc8a65bb92
Kostadin Golev <kotseto@gmail.com>
2014-09-23 09:07:46 +0300

    some catalog design (still work in progress)

--- js/controllers.js
+++ js/controllers.js
@@ -11,18 +11,60 @@ var hopesoapApp = angular.module('hopeSoapApp', []);
 
 
 hopesoapApp.controller('SoapListCtrl', function ($scope) {
+  $scope.categories = [
+  	"Класически",
+  	"Луксозни",
+  	"Коледа",
+  	"Специални случаи",
+  	"Фирмени"
+  ];
+
+  $scope.orders = [
+  	{'name': 'Luxury',
+  	 'price': 5,
+     'count': 2,
+     'total': 10 },
+  	{'name': 'Christmas special',
+  	 'price': 8,
+     'count': 1,
+     'total': 8 }
+  ];
+
   $scope.soaps = [
     {'name': 'Luxury',
      'description': 'Луксозни сапуни за вашия специален повод',
      "imageUrl": "img/soaps/soap1.jpg", 
- 	 'createdAt': new Date('12/23/2013')},
+ 	 'createdAt': new Date('12/23/2013'),
+ 	 "price": 5},
     {'name': 'Christmas special',
      'description': 'Коледа',
      "imageUrl": "img/soaps/soap2.jpg",
- 	 'createdAt': new Date('11/24/2013')},
+ 	 'createdAt': new Date('11/24/2013'),
+ 	 "price": 8},
     {'name': 'Classic',
      'description': 'Класически модели сапуни',
      "imageUrl": "img/soaps/soap2.jpg",
- 	 'createdAt': new Date('07/08/2014')},
+ 	 'createdAt': new Date('07/08/2014'),
+ 	 "price": 1},
+    {'name': 'Classic',
+     'description': 'Класически модели сапуни',
+     "imageUrl": "img/soaps/soap2.jpg",
+ 	 'createdAt': new Date('07/08/2014'),
+ 	 "price": 1},
+    {'name': 'Classic',
+     'description': 'Класически модели сапуни',
+     "imageUrl": "img/soaps/soap2.jpg",
+ 	 'createdAt': new Date('07/08/2014'),
+ 	 "price": 1},
+    {'name': 'Classic',
+     'description': 'Класически модели сапуни',
+     "imageUrl": "img/soaps/soap2.jpg",
+ 	 'createdAt': new Date('07/08/2014'),
+ 	 "price": 1},
+    {'name': 'Classic',
+     'description': 'Класически модели сапуни',
+     "imageUrl": "img/soaps/soap2.jpg",
+ 	 'createdAt': new Date('07/08/2014'),
+ 	 "price": 1}
   ];
 });
