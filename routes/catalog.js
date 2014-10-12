8d2a41de802d01f32e32fe49902aaa0350d83a39
branimira <b.l.todorova@gmail.com>
2014-10-12 22:40:17 +0300

    move file upload to catalog route file, create admin.html page

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var inspect = require('util').inspect;
+var Busboy = require('busboy');
+var os = require('os');
+var fs = require('fs');	
 
 //the url with param in it should be before the generic one, it doesn't work the other way
 router.get('/soaps/:id', function(req, res) {
@@ -68,5 +71,47 @@ router.delete('/soaps/:id', function(req, res) {
 		});
 	});
 });
+
+
+router.post('/soap/upload', function(req, res) {
+    uploadFile(req, res);
+	res.redirect('/../admin.html');
+});
+
+
+function uploadFile(req, res) {
+	var soap = {};
+	var busboy = new Busboy({ headers: req.headers });
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+        var target_path = __dirname + '/../public/img/soaps/' + filename;		
+		soap['filePath'] = filename;
+		file.pipe(fs.createWriteStream(target_path));
+    });
+    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
+		soap[fieldname] = val;
+		console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+    });
+    busboy.on('finish', function() {
+      console.log('Done parsing form!');
+	  console.log(soap);
+	  saveSoap(req, res, soap);
+    });
+    req.pipe(busboy);
+}
+
+function saveSoap(req, res, soap) {
+	var db = req.db;	
+	db.collection('soaps', function(err, collection) {
+		collection.insert(soap, {safe:true}, function(err, result) {
+		if (err) {
+			res.send({'error':'An error has occurred'});
+		} else {
+			console.log('Success: ' + JSON.stringify(result[0]));
+			//res.send(result[0]);
+		}
+		});
+	});
+
+}
 
 module.exports = router;
--- web.js
+++ web.js
@@ -1,7 +1,6 @@
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var fs = require('fs');
 
 var MongoClient = require('mongodb').MongoClient;
 var Server = require('mongodb').Server;
@@ -34,65 +33,6 @@ mongoClient.open(function(err, mongoClient){
 		console.log("server has started");
 });
 
-
-var form = "<!DOCTYPE HTML><html><body>" +
-"<form method='post' action='/upload' enctype='multipart/form-data'>" +
-"<input type='text' name='soapName' value=''/>" +
-"<input type='text' name='quantity' value=''/>" +
-"<input type='text' name='category' value=''/>" +
-"<input type='file' name='image'/>" +
-"<input type='submit' /></form>" +
-"</body></html>";
-
-app.get('/upload', function (req, res){
-	res.writeHead(200, {'Content-Type': 'text/html' });
-	res.end(form);
-
-});
-
-app.post('/upload', function(req, res) {
-    uploadFile(req, res);
-	res.redirect('/upload');
-});
-
-var inspect = require('util').inspect;
-var Busboy = require('busboy'),
-os = require('os');
-	
-function uploadFile(req, res) {
-	var soap = {};
-	var busboy = new Busboy({ headers: req.headers });
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-        var target_path = __dirname + '/public/img/soaps/' + filename;		
-		soap['filePath'] = target_path;
-		file.pipe(fs.createWriteStream(target_path));
-    });
-    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
-		soap[fieldname] = val;
-		console.log('Field [' + fieldname + ']: value: ' + inspect(val));
-    });
-    busboy.on('finish', function() {
-      console.log('Done parsing form!');
-	  console.log(soap);
-	  saveSoap(req, res, soap);
-    });
-    req.pipe(busboy);
-}
-
-function saveSoap(req, res, soap) {
-	var db = req.db;	
-	db.collection('soaps', function(err, collection) {
-		collection.insert(soap, {safe:true}, function(err, result) {
-		if (err) {
-			res.send({'error':'An error has occurred'});
-		} else {
-			console.log('Success: ' + JSON.stringify(result[0]));
-			//res.send(result[0]);
-		}
-		});
-	});
-
-}
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
